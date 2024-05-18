4c112a269b2723e73b2f76ca3ab924669642b778
unknown <renzarcilla09@gmail.com>
2024-05-18 17:51:28 +0800

    created pseudocode

---
+++ test_TV.py
@@ -0,0 +1,35 @@
+# pseudocode:
+#     start:
+#         create a class named TV
+#             create constructor
+#                 create parameters for channel, volume, and on_or_off
+#             create method for turning the TV on
+#             create method for turning the TV off
+#             create method that returns the current channel of the TV object
+#             create method that sets a new channel for the TV object
+#             create method that returns the current volume of the TV object
+#             create method that sets a new volume for the TV object
+#             create method that increases the channel number of the TV object by 1
+#             create method that increases the volume of the TV object by 1
+#             create method that decreases the volume of the TV object by 1
+#
+#         create an object named tv1
+#             turn tv1 on
+#             set channel of tv1 to 30
+#             increase channel of tv1 by 1
+#             decrease channel of tv1 by 1
+#             set volume of tv1 to 3
+#             increase volume of tv1 by 1
+#             decrease volume of tv1 by 1
+#             get the current channel and volume of tv1
+#         create an object named tv2
+#             turn tv2 on
+#             set channel of tv2 to 3
+#             increase channel of tv2 by 1
+#             decrease channel of tv2 by 1
+#             set volume of tv2 to 2
+#             increase volume of tv2 by 1
+#             decrease volume of tv2 by 1
+#             get the current channel and volume of tv2
+#     end:
+
