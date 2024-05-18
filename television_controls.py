f365dab4a0d8dc71de105881180c196009d005db
unknown <renzarcilla09@gmail.com>
2024-05-18 19:10:25 +0800

    created methods to set new channel and another for getting current channel

--- television_controls.py
+++ television_controls.py
@@ -13,3 +13,15 @@ class TV():  # class
     def turn_off(self):
         print("object " + "is currently off")
 
+    # create method that sets a new channel for the TV object
+    def set_new_channel(self):
+        while True:
+            try:
+                self.channel = int(input("Set new channel for object: "))
+                break  # Exit the loop if user inputted a valid int
+            except ValueError:
+                print("You've submitted an invalid value! Try again.")
+
+    # create method that returns the current channel of the TV object
+    def get_current_channel(self):
+        return self.channel
