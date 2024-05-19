fd2f49ac5c6006225b48a6a2d6dd393c35fc1306
unknown <renzarcilla09@gmail.com>
2024-05-19 16:23:45 +0800

    catched error in block of code that sets new volume

--- test_TV.py
+++ test_TV.py
@@ -96,6 +96,7 @@ off_or_on = "this_is_a_wrong_input"
 user_channel = "this_is_a_wrong_input"
 user_channel_up = "this_is_a_wrong_input"
 user_channel_down = "this_is_a_wrong_input"
+user_volume = "this_is_a_wrong_input"
 
 while off_or_on not in valid_inputs:
     off_or_on = input("Would you like to turn tv1 on (yes/no)? ")
@@ -141,9 +142,16 @@ while off_or_on not in valid_inputs:
                     else:
                         print("You've inputted an invalid value. Choose again")
 
-                user_volume = input("Would you like to input a new volume (yes/no)? ")
-                if user_volume == "yes":
-                    tv1.set_new_volume()
+                while user_volume not in valid_inputs:
+                    if user_volume == "yes":
+                        tv1.set_new_volume()
+                        print("")
+                        break
+                    elif user_volume == "no":
+                        break
+                    else:
+                        print("You've inputted an invalid value. Choose again")
+
                 user_volume_up = input("Would you like to increase volume by 1 (yes/no)? ")
                 if user_volume == "yes":
                     tv1.volume_up()
